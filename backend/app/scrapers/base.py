ec41bcef5a17e44c535038d5222f61f4b90f821a
Paya <alizeayn@gmail.com>
2026-07-01 19:44:12 +0330

    feat: add base scraper

--- backend/app/scrapers/base.py
+++ backend/app/scrapers/base.py
@@ -114,12 +114,3 @@ class BaseScraper(abc.ABC):
     
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} source='{self.source_name}'>"
-
-        
-        
-    
-
-
-
-    
-        
